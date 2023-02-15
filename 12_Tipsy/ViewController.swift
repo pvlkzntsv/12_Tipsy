1ee82212a4a5f23ca543494d4c2fd8a302da8a0b
pvlkzntsv <pvlkzntsv@yandex.ru>
2023-02-15 15:30:15 +0700

    Initial Commit

---
+++ 12_Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12_Tipsy
+//
+//  Created by pvl kzntsv on 15.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
